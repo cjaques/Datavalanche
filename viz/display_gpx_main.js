88fe4770fac8a9b7535bd5517715a0041a565425
Christian Jaques <christian.jaques@gmail.com>
2018-11-29 10:15:49 +0100

    display_gpx_main.js displays list of gpx on the Swiss map. TODO : add pan an zoom actions + make line instead of list of points

--- viz/display_gpx_main.js
+++ viz/display_gpx_main.js
@@ -58,28 +58,14 @@ class MapPlot {
             .style('stroke-width', '1px')
     }
 
-
-    // interesting, see --> https://github.com/chrissng/d3.gpx
-    readGPX(file) {
-        
-        var rnd = d3.xml(file, function(data){
-            
-            // Load GPX data by looking at xml file
-            d3.select(data).selectAll("rtept").each(function(item) {
-                console.log(item)
-                var lat = parseFloat(d3.select(this).attr("lat"));
-                var lon = parseFloat(d3.select(this).attr("lon"));
-
-                latList.push(lat);
-                lonList.push(lon);
-                this.lineString.push([lon, lat]);
-            });
-        });
-        console.log("---- function done : res --");
-        console.log(this.lineString);
+    redraw(transform) {
+        context.clearRect(0, 0, this.width, this.height);
+        context.save();
+        if (transform) {
+          context.translate(transform.x, transform.y);
+          context.scale(transform.k, transform.k);    
+        }
     }
-
-
 
     constructor(svg_element_id) {
         this.svg = d3.select('#' + svg_element_id);
@@ -89,39 +75,41 @@ class MapPlot {
         this.svg_width = svg_viewbox.width;
         this.svg_height = svg_viewbox.height;
 
+        const file_list = ["data/morcles.gpx", "data/mumi.gpx", "data/mu.gpx"];
+        var all_promises = [];
 
-        const gpx_promise = d3.xml("data/morcles.gpx").then((data) =>{
-            let latList = [];
-            let lonList = [];
-            let lineString = [];
-
-            d3.select(data).selectAll("rte").selectAll("rtept").each(function() {
-                var lat = parseFloat(d3.select(this).attr("lat"));
-                var lon = parseFloat(d3.select(this).attr("lon"));
-                latList.push(lat);
-                lonList.push(lon);
-                lineString.push([lon, lat]);
-            });
-            return lineString;
-        });
-
-        const map_promise = d3.json("data/ch-cantons.json").then((topojson_raw) => {
+        all_promises.push(d3.json("data/ch-cantons.json").then((topojson_raw) => {
             const canton_paths = topojson.feature(topojson_raw, topojson_raw.objects.cantons);
             return canton_paths.features;
-        });
+        }));
 
-        const point_promise = d3.csv("data/locations.csv").then((data) => {
-            
-            return data;
-        });
+        // go through all items in list 
+        for(var i=0; i < file_list.length; i++){
+            all_promises.push(d3.xml(file_list[i]).then((data) =>{
+                // interesting way to parse XML --> https://github.com/chrissng/d3.gpx
+                let lineString = [];
+                // GPX files are filled in various ways
+                d3.select(data).selectAll("rte").selectAll("rtept").each(function() {
+                    var lat = parseFloat(d3.select(this).attr("lat"));
+                    var lon = parseFloat(d3.select(this).attr("lon"));
+                    lineString.push([lon, lat]);
+                });
+                d3.select(data).selectAll("trkseg").selectAll("trkpt").each(function() {
+                    var lat = parseFloat(d3.select(this).attr("lat"));
+                    var lon = parseFloat(d3.select(this).attr("lon"));
+                    lineString.push([lon, lat]);
+                });
+                return lineString;
+            }));
+        };
 
-        Promise.all([map_promise, point_promise, gpx_promise]).then((results) => {
+        // Once all data loading promises have been solved
+        Promise.all(all_promises).then((results) =>{
             let map_data = results[0];
-            let point_data = results[1];
-            let gpx_data = results[2];
-
-
-            console.log('Data loaded', gpx_data);
+            let gpx_data = [];
+            for (i=0; i< file_list.length; i++){
+                gpx_data.push(results[i+1]);
+            }
 
             // Declare projection
             var projection = d3.geoMercator() // d3.geoEqualEarth()
@@ -130,7 +118,10 @@ class MapPlot {
                 .translate([500, 275]);
             var path = d3.geoPath().projection(projection);
 
-            // // get max value from cantonId_to_population <-- IMPROVE THIS
+            // catch cannvas and add zoom function
+            // on("zoom", function () {redraw(d3.event.transform);});
+
+            // get max value from cantonId_to_population <-- IMPROVE THIS
             // var max = 0;
             // var min = 100000;
             // for(var i=0; i<cantonId_to_population.length; i++){
@@ -142,11 +133,13 @@ class MapPlot {
             //         min = v;
             //     }
             // }
+
             var max = 6000;
             var min = 3;
 
             // color scale
             //var color_scale = d3.interpolateViridis; //.domain([0, max]);
+
             var color_scale = d3.scaleLog()
                 .domain([min, max])
                 .range([d3.rgb('#453ef0'), d3.rgb('#0be8f0')])
@@ -159,27 +152,31 @@ class MapPlot {
                 .attr('class','canton');
                 // .style("fill", function(d, i){return color_scale(cantonId_to_population[i].density)});
 
-            // Draw the canton labels
-            this.svg.selectAll("text")
-                .data(map_data)
-                .enter()
-                .append("text")
-                .text(function(d){return d.id}) // return d.properties["name"]})  <-- prints full canton name
-                .attr("x", function(d){return path.centroid(d)[0]})
-                .attr("y", function(d){return path.centroid(d)[1]})
-                .attr("class", "canton-label");
+            // // Draw the canton labels
+            // this.svg.selectAll("text")
+            //     .data(map_data)
+            //     .enter()
+            //     .append("text")
+            //     .text(function(d){return d.id}) // return d.properties["name"]})  <-- prints full canton name
+            //     .attr("x", function(d){return path.centroid(d)[0]})
+            //     .attr("y", function(d){return path.centroid(d)[1]})
+            //     .attr("class", "canton-label");
 
 
-            // Draw the points
-            this.svg.selectAll("circle")
-                .data(gpx_data)
+            // Draw the GPX points
+            for(var i = 0; i<file_list.length; i++){
+                console.log("Mimu i ", i, " data ", gpx_data[i]);
+                this.svg.selectAll("circle")
+                .data(gpx_data[i])
                 .enter()
                 .append("circle")
-                .attr("cx", function(d){return projection(d)[0];}) // [+d.lon, +d.lat])[0];})
+                .attr("cx", function(d){console.log(" : ", d); return projection(d)[0];}) // [+d.lon, +d.lat])[0];})
                 .attr("cy", function(d){return projection(d)[1];}) //function(d){return projection([+d.lon, +d.lat])[1];})
                 .attr("r", 1)
                 .style("fill", "#ff6b06")
                 .style("opacity", 0.5);
+            }
+    
 
             // add colorbar to drawing
             this.makeColorbar(this.svg, color_scale, [50, 30], [20, this.svg_height - 2*30]);
