a67d31f37d78f2e47d9bf159d3ab7996f46cc999
Christian Jaques <christian.jaques@gmail.com>
2018-12-19 15:53:07 +0100

    Display all GPS points on the map, clicking on them displays the name of the outing

--- viz/gpx_leaflet_main.js
+++ viz/gpx_leaflet_main.js
@@ -1,5 +1,5 @@
 // 
-// C.Jaques, christian.jaques@idiap.ch, November 2018
+// Copyright T. Kulak, E. Pignat, C.Jaques, name DOT surname AT idiap DOT ch, December 2018
 //
 // Inspired from DataViz exercise session 8, 
 //      http://bl.ocks.org/d3noob/9267535, 
@@ -16,7 +16,7 @@ class MapPlot {
     constructor(svg_element_id) {
         map = L.map('map').setView([46.8, 8.2], 8); // center and zoom
         const mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
-        L.tileLayer(
+        this.mainLayer = L.tileLayer(
             'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; ' + mapLink + ' Contributors',
             maxZoom: 16,
@@ -25,24 +25,43 @@ class MapPlot {
         // Initialize the SVG layer
         map._initPathRoot()    
 
+        // // TODO : DEBUG BELOW -- trying to add a sliderControl
+        // // slider control
+        // var sliderControl = L.control.sliderControl({position: "topright", layer: this.mainLayer});
+        // //Make sure to add the slider to the map ;-)
+        // map.addControl(sliderControl);
+        // //And initialize the slider
+        // sliderControl.startSlider();
+
         // Pick up the SVG from the map object
         this.svg = d3.select("#map").select("svg");
         this.g = this.svg.append("g");
 
 
-        const file_list = ["data/morcles.gpx", "data/mumi.gpx", "data/mu.gpx"];
+        const file_list =  read_text_file("data/files.txt"); // read data/files.txt  ["data/morcles.gpx", "data/mumi.gpx", "data/mu.gpx"];
+        var array_list = file_list.split("\n"); //  JSON.parse("[" + file_list.split("\n") + "]");
+
+
         let all_promises = [];
         let idx = 0;
 
         // go through all items in list 
-        for(var i=0; i<file_list.length; i++){       
+        for(var i=0; i<array_list.length -1; i++){       
             all_promises.push(new Promise(function(resolve, reject){
-                d3.xml(file_list[i], function(data){
+                d3.xml("data/" + array_list[i], function(data){
                     // interesting way to parse XML --> https://github.com/chrissng/d3.gpx
                     let lineString = [];
                     let nameli = ""; // why not Swiss German ?
 
-                    // first, read name of outing
+                    // read name of outing
+                    d3.select(data).selectAll("rte").selectAll("name").each(function(){
+                        nameli = this.innerHTML;
+                    })
+                    d3.select(data).selectAll("trk").selectAll("name").each(function(){
+                        nameli = this.innerHTML;
+                    })
+
+                    // similarly, try to get the outing's date (not all GPX have the date included)
                     d3.select(data).selectAll("rte").selectAll("name").each(function(){
                         nameli = this.innerHTML;
                     })
@@ -70,9 +89,9 @@ class MapPlot {
         // // Once all data loading promises have been solved
         Promise.all(all_promises).then((results) =>{
             let gpx_data = [];
-            for (i=0; i< file_list.length; i++){
+            for (i=0; i< array_list.length-1; i++){
                 gpx_data.push(results[i]);
-                console.log(gpx_data[i]);
+                console.log("i : ", i, " ", gpx_data[i]);
             }
 
             // Draw the GPX points
@@ -82,14 +101,31 @@ class MapPlot {
              for(var i = 1; i<gpx_data.length; i++){
                  gpx_data_flat = gpx_data_flat.concat(gpx_data[i]);
              }  
+
+
             feature = this.svg.selectAll("circle")
             .data(gpx_data_flat)
             .enter()
             .append("circle")
-            .attr("r", 5)
+            .attr("r", 4)
             .style("fill", "#ff6b06")
-            .style("opacity", 0.8);
+            .style("opacity", 0.8)
+            .on("click", function(d){
+                var popup = L.popup()
+                            .setLatLng([d['lat'], d['lon']]) // latlng)
+                            .setContent(d['name'])
+                            .openOn(map);
+                var marker = L.marker().addTo(map);
+                marker.setOpacity(0);
+                marker.bindPopup(d['name']).openPopup();
+            });
 
+            // var points_array = [];
+            // for(var i =0; i<gpx_data_flat.length; i++){
+            //     points_array.push([gpx_data_flat[i]['lat'], gpx_data_flat[i]['lon'], 0.1])
+            // }
+            // var heat = L.heatLayer(points_array, {radius: 4}).addTo(map);
+            
             map.on("viewreset", this.update);
             this.update();
         });
@@ -122,3 +158,21 @@ whenDocumentLoaded(() => {
 });
 
 
+
+// ------------------------------------------------------------------------ HELPER FUNCTIONS
+
+function read_text_file(filename){
+    var return_text;
+    var rawfile = new XMLHttpRequest();
+    rawfile.open("GET", filename, false);
+    rawfile.onreadystatechange = function (){
+        if(rawfile.readyState === 4){
+            if(rawfile.status === 200){
+                return_text = rawfile.responseText;
+            }
+        }
+    }
+    rawfile.send(null);
+
+    return return_text;
+}
